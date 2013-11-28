862998e1f1fc1b0f92b22fbbdb647525844ceff8
Isao Isaac Saito <130s@lateeye.net>
2013-11-28 19:20:47 +0900

    Rebase from 'upstream'

--- jenkins-catkin-devel.sh
+++ jenkins-catkin-devel.sh
@@ -17,12 +17,16 @@ DISTRO=${1:-groovy}
 source /opt/ros/${DISTRO}/setup.bash
 setup
 cd catkin_ws
+if [ "${DISTRO}" == "hydro" ]; then
+    (cd src; wstool set robot_model https://github.com/ros/robot_model.git --git -y; wstool update robot_model)
+fi
 rosdep update
 rosdep install --reinstall --from-paths src --ignore-src --rosdistro ${DISTRO} -y -r
 catkin_make
 
 source devel/setup.bash
 source `rospack find openrtm_tools`/scripts/rtshell-setup.sh
+trap 'rosrun rosunit clean_junit_xml.py' ERR
 rtmtest hironx_ros_bridge test-hironx.launch
 rtmtest hironx_ros_bridge test-hironx-ros-bridge.launch
 
