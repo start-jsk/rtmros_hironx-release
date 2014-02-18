252df5eadbe50fa6df9cea371528185606b0ae4c
Isao Isaac Saito <130s@lateeye.net>
2014-02-19 04:24:30 +0900

    Imported upstream version '1.0.11' of 'upstream'

--- hironx_ros_bridge/scripts/hironx.py
+++ hironx_ros_bridge/scripts/hironx.py
@@ -9,7 +9,7 @@ except:
 
 from hironx_ros_bridge import hironx_client
 
-import rtm
+from hrpsys import rtm  # See https://github.com/tork-a/rtmros_nextage/commit/d4268d81ec14a514bb4b3b52614c81e708dd1ecc#diff-20257dd6ad60c0892cfb122c37a8f2ba 
 import argparse
 
 if __name__ == '__main__':
@@ -33,14 +33,17 @@ if __name__ == '__main__':
     if len(unknown) >= 2:
         args.robot = unknown[0]
         args.modelfile = unknown[1]
-    hiro = hironx_client.HIRONX()
-    hiro.init(robotname=args.robot, url=args.modelfile)
+    robot = hiro = hironx_client.HIRONX()
+    robot.init(robotname=args.robot, url=args.modelfile)
 
 # for simulated robot
 # $ ./hironx.py
 #
 # for real robot
-# ../script/hironx.py  --host hiro014
-# ./ipython -i hironx.py --host hiro014
+# ../script/hironx.py -- --host hiro014
+# ./ipython -i hironx.py -- --host hiro014
 # for real robot with custom model file
-# ../script/hironx.py  --host hiro014 --modelfile /opt/jsk/etc/HIRONX/model/main.wrl
+# ../script/hironx.py -- --host hiro014 --modelfile /opt/jsk/etc/HIRONX/model/main.wrl
+#
+# See http://unix.stackexchange.com/questions/11376/what-does-double-dash-mean
+# for the use of double-dash on unix/linux.
