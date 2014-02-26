1c3f0c2d351b1902a89bd0fe17d35d63799a83d5
Isaac Isao Saito <130s@lateeye.net>
2014-02-26 12:13:33 +0900

    Rebase from 'debian/hydro/hironx_ros_bridge'

--- robot/check/md5sum-pkg.sh
+++ robot/check/md5sum-pkg.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 python <<EOF
 
 import hashlib
@@ -10,16 +9,16 @@ from operator import add
 print "* Check /usr/pkg files"
 
 f = open('/tmp/check-usr-pkg-md5.txt', 'w')
-for dir in [['/usr/pkg/bin',    'e846c663623045c0846353f8d1187c0c'],
-            ['/usr/pkg/include','461ea43f9de05ef519c487f5e84bd988'],
-            ['/usr/pkg/lib',    'cc0f135dc109309132292fe2834afbda'],
-            ['/usr/pkg/sbin',   'c85c04e21d100b9a9d40da813ff2ec04'],
-            ['/usr/pkg/etc',    '6fa3a22c96ae700e95c3007f2da3cc11'],
-            ['/usr/pkg/info',   '56e7ec0e27be98c0e51f17cd21b652b8'],
-            ['/usr/pkg/man',    '9b9a2be866fd83d13d826657fba8367f'],
-            ['/usr/pkg/share',  '1aff998cad709c0a8d8f95138b01e5a5']]:
+for dir in [['/usr/pkg/bin',    0x3d8eff565b05b7532b3107b20589cbe6L],
+            ['/usr/pkg/include',0xcf89f4f17f1c569afc8ec3ad8a5ec10aL],
+            ['/usr/pkg/lib',    0x5facb204c74dce823ebe7fef06d80cffL],
+            ['/usr/pkg/sbin',   0x58a3b20eafea4170215f820182f68483L],
+            ['/usr/pkg/etc',    0xbeef94e03b158101a77526be67d3b9ecL],
+            ['/usr/pkg/info',   0x3e0519bf78f3258cc72f3d095e942ed8L],
+            ['/usr/pkg/man',    0xb97c0f124a4170abd7c170fc882b7425L],
+            ['/usr/pkg/share',  0x814de4e7c790d11dc4764139b5fb3625L]]:
   full_dir = dir[0]
-  dir_md5 = hashlib.md5()
+  dir_md5 = 0
   for root, dirs, files in os.walk(full_dir):
     for file in files:
       if os.path.exists(os.path.join(root,file)):
@@ -28,8 +27,8 @@ for dir in [['/usr/pkg/bin',    'e846c663623045c0846353f8d1187c0c'],
         file_md5 = 'No such file or directory'
         print "  ** ",os.path.join(root,file),"\t\t",file_md5
       print >>f, os.path.join(root,file),"\t\t",file_md5
-      dir_md5.update(file_md5)
-  print "  Check ", full_dir, "   \t(", dir_md5.hexdigest(), ")\t", dir_md5.hexdigest()==dir[1]
+      dir_md5 = dir_md5 ^ int(file_md5, 16)
+  print "  Check ", full_dir, "   \t(", hex(dir_md5), ")\t", dir_md5==dir[1]
 f.close
 #  for file in files
 # print [hashlib.md5(open(fname, 'rb').read()).hexdigest() for fname in reduce(add, [[os.path.join(root, file) for file in files] for root, dirs, files in os.walk('$dir')])]"
