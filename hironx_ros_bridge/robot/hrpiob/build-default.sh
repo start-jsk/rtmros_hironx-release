1ecb32cf6ef66d3609f8237717dd09da73ee4310
Kei Okada <kei.okada@gmail.com>
2016-02-12 00:02:50 +0900

    Trimmed the branch to only the hironx_moveit_config sub directory

--- hironx_ros_bridge/robot/hrpiob/build-default.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-rm -rf _build
-mkdir _build
-(cd _build ; cmake -DCMAKE_INSTALL_PREFIX=/opt/nextage-open ..; make VERBOSE=1; make install)
-#(cd _build ; cmake -DCMAKE_INSTALL_PREFIX=/tmp/hrpsys-base ..; make VERBOSE=1; make install)
