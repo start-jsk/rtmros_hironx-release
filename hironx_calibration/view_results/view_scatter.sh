b3c1dbbe56c06dce2b7934b19e6a113b2dcf6cb0
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-08-10 11:42:17 +0900

    Trimmed the branch to only the rtmros_hironx sub directory

--- hironx_calibration/view_results/view_scatter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosrun calibration_estimation error_visualization.py /tmp/hironx_calibration/cal_measurements.bag /tmp/hironx_calibration/ `rospack find hironx_calibration`/view_results/scatter_config.yaml
